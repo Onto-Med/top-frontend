ecf1b096ce58d599fc80345452ce36f1d7bcf669
Christoph Beger <ChristophB@users.noreply.github.com>
2021-10-25 18:29:46 +0000

    Finally fix tests

--- test/jest/__tests__/LocalizedTextInput.spec.js
+++ test/jest/__tests__/LocalizedTextInput.spec.js
@@ -8,31 +8,33 @@ installQuasarPlugin();
 
 describe('LocalizedTextInput.vue', () => {
   it('adds empty localized string', async () => {
-    const titles: Record<string, unknown>[] = []
     const wrapper = mount(LocalizedTextInput, {
-      props: { modelValue: titles }
+      props: { modelValue: [] }
     })
     expect(wrapper).toBeTruthy()
     expect(wrapper.find('button.remove-localized-text-btn').exists()).toBeFalsy()
     const btn = wrapper.find('button.add-localized-text-btn')
     expect(btn.exists()).toBeTruthy()
     await btn.trigger('click')
-    expect(titles.length).toBe(1)
+    let emitted = wrapper.emitted('update:modelValue')[0][0]
+    expect(emitted.length).toBe(1)
   })
 
   it('removes localized string', async () => {
-    const titles = [
-      { lang: 'de', text: 'Beispiel' },
-      { lang: 'en', text: 'Example' }
-    ]
     const wrapper = mount(LocalizedTextInput, {
-      props: { modelValue: titles }
+      props: {
+        modelValue: [
+          { lang: 'de', text: 'Beispiel' },
+          { lang: 'en', text: 'Example' }
+        ]
+      }
     })
     expect(wrapper).toBeTruthy()
     const btns = wrapper.findAll('button.remove-localized-text-btn')
     expect(btns.length).toBe(2)
     expect(btns[1].exists()).toBeTruthy()
     await btns[1].trigger('click')
-    expect(titles.length).toBe(1)
+    let emitted = wrapper.emitted('update:modelValue')[0][0]
+    expect(emitted.length).toBe(1)
   })
 })
