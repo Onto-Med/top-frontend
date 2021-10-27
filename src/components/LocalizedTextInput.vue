4f4a4333c21664939847e5ce08e7eabb8823657a
Christoph Beger <ChristophB@users.noreply.github.com>
2021-10-27 11:59:25 +0000

    Move localized text input into card

--- src/components/LocalizedTextInput.vue
+++ src/components/LocalizedTextInput.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="q-pa-md">
-    <div class="row" v-for="(entry, index) in modelValue" :key="index">
-      <q-input outlined :type="textArea ? 'textarea' : 'text'" :rows="rows" :cols="cols" v-model="entry.text">
-        <template v-slot:before>
-          <q-select outlined v-model="entry.lang" :options="supportedLangs" />
-        </template>
-        <template v-slot:after>
-          <q-btn color="red" icon="remove" class="remove-localized-text-btn" @click="removeEntryByIndex(index)" />
-        </template>
-      </q-input>
-    </div>
-    <q-btn color="primary" icon="add" class="add-localized-text-btn" @click="addEntry()" :label="`Add new ${name}`" />
-    <div v-show="uniqueLangs && duplicatedLangs.length > 0">There can only be one {{ name }} per language!</div>
-  </div>
+  <q-card flat bordered class="q-pa-md bg-grey-1">
+    <q-card-section v-if="label">
+      <div class="text-h6">{{ label }}</div>
+    </q-card-section>
+
+    <q-separator v-if="label" />
+
+    <q-card-section>
+      <div class="row" v-for="(entry, index) in modelValue" :key="index">
+        <q-input outlined :type="textArea ? 'textarea' : 'text'" :rows="rows" :cols="cols" v-model="entry.text">
+          <template v-slot:before>
+            <q-select outlined v-model="entry.lang" :options="supportedLangs" />
+          </template>
+          <template v-slot:after>
+            <q-btn color="red" icon="remove" class="remove-localized-text-btn" @click="removeEntryByIndex(index)" />
+          </template>
+        </q-input>
+        <div v-show="uniqueLangs && duplicatedLangs.length > 0">There can only be one {{ name }} per language!</div>
+      </div>
+    </q-card-section>
+
+    <q-separator />
+
+    <q-card-actions>
+      <q-btn color="primary" icon="add" class="add-localized-text-btn" @click="addEntry()" :label="`Add new ${name}`" />
+    </q-card-actions>
+  </q-card>
 </template>
 
 <script lang="ts">
@@ -32,6 +45,7 @@ export default defineComponent({
       type: String,
       default: 'entry'
     },
+    label: String,
     supportedLangs: {
       type: Array as () => string[],
       default: () => ['en']
