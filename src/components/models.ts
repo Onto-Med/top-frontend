b67bdcde40a109503d10abb0899948813f6f0709
Christoph Beger <ChristophB@users.noreply.github.com>
2021-10-27 12:42:05 +0000

    Define phenotype interface and entity type enum

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,8 +1,21 @@
-export interface Todo {
-  id: number;
-  content: string;
+export interface IPhenotype {
+  id: string;
+  title: string;
+  entityType: EntityType;
+  titles: Array<Record<string, string>>;
+  datatype: string;
+  synonyms: Array<Record<string, string>>;
+  descriptions: Array<Record<string, string>>;
 }
 
-export interface Meta {
-  totalCount: number;
+enum EntityType {
+  category = 'category',
+  missing_value_phenotype = 'missing_value_phenotype',
+  group_phenotype = 'group_phenotype',
+  unrestricted_single_phenotype = 'unrestricted_single_phenotype',
+  restricted_single_phenotype = 'restricted_single_phenotype',
+  unrestricted_combined_phenotype = 'unrestricted_combined_phenotype',
+  restricted_combined_phenotype = 'restricted_combined_phenotype',
+  unrestricted_derived_phenotype = 'unrestricted_derived_phenotype',
+  restricted_derived_phenotype = 'restricted_derived_phenotype'
 }
