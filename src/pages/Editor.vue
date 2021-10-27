baaf9df47c60cf7fcf5032b2e50ee0309dc18735
Christoph Beger <ChristophB@users.noreply.github.com>
2021-10-27 14:55:34 +0000

    Add tree and move editor into tab

--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -1,17 +1,34 @@
 <template>
   <q-splitter v-model="splitterModel">
     <template #before>
-      tree
+      <div class="q-pa-md">
+        <q-tree
+        ref="tree"
+          :nodes="phenotypeNodes"
+          node-key="id"
+          selected-color="primary"
+          v-model:expanded="treeExpansion"
+          v-model:selected="selectedNode"
+        />
+      </div>
     </template>
     <template #after>
-      <phenotype-editor :entityType="entityType" />
+      <q-tabs v-model="tabModel" dense align="left" class="bg-primary text-white shadow-2">
+        <q-tab :name="tab.id" :label="tab.label" v-for="tab in tabs" :key="tab.id" />
+      </q-tabs>
+      <q-tab-panels v-model="tabModel">
+        <q-tab-panel :name="tab.id" v-for="tab in tabs" :key="tab.id">
+          <phenotype-editor :entityType="entityType" :entityId="tab.id" />
+        </q-tab-panel>
+      </q-tab-panels>
     </template>
   </q-splitter>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { EntityType } from 'src/components/models';
+import { IPhenotypeTreeNode, EntityType } from 'src/components/models'
+import { QTree } from 'quasar'
 import PhenotypeEditor from 'src/components/PhenotypeEditor.vue'
 
 export default defineComponent({
@@ -23,8 +40,45 @@ export default defineComponent({
     return {
       showJson: false,
       splitterModel: 25,
-      entityType: EntityType.UnrestrictedSinglePhenotype
+      entityType: EntityType.UnrestrictedSinglePhenotype,
+      phenotypeNodes: [] as IPhenotypeTreeNode[],
+      treeExpansion: [],
+      selectedNode: String,
+      tabModel: '',
+      tabs: [] as IPhenotypeTreeNode[]
     }
+  },
+  watch: {
+    selectedNode (key: string) {
+      if (key && !this.tabs.map((t: IPhenotypeTreeNode) => t.id).includes(key)) {
+        let tree = this.$refs.tree as QTree
+        this.tabs.push(tree.getNodeByKey(key) as IPhenotypeTreeNode)
+        this.tabModel = key
+      }
+    }
+  },
+  mounted () {
+    // TODO: load phenotype nodes
+    this.phenotypeNodes = [
+      {
+        id: 1,
+        label: 'Anthropometry',
+        children: [
+          { id: 3, label: 'BMI', icon: 'calculate' },
+          { id: 4, label: 'Height', icon: 'calculate' },
+          {
+            id: 5,
+            label: 'Weight',
+            icon: 'calculate',
+            children: [
+              { id: 6, label: '1500-2500g' },
+              { id: 7, label: '>80kg' }
+            ]
+          }
+        ]
+      },
+      { id: 2, label: 'Laboratory values' }
+    ]
   }
 })
 </script>
