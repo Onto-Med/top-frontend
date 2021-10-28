aaa2dbc8ac27a520c98c22bdf7f4c3d683ef14f8
Christoph Beger <ChristophB@users.noreply.github.com>
2021-10-28 16:19:01 +0000

    Update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,12 +39,12 @@ module.exports = {
     // but leave only one uncommented!
     // See https://eslint.vuejs.org/rules/#available-rules
     'plugin:vue/vue3-essential', // Priority A: Essential (Error Prevention)
-    // 'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
-    // 'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
+    'plugin:vue/vue3-strongly-recommended', // Priority B: Strongly Recommended (Improving Readability)
+    'plugin:vue/vue3-recommended', // Priority C: Recommended (Minimizing Arbitrary Choices and Cognitive Overhead)
 
     // https://github.com/prettier/eslint-config-prettier#installation
     // usage with Prettier, provided by 'eslint-config-prettier'.
-    'prettier'
+    // 'prettier'
   ],
 
   plugins: [
@@ -76,6 +76,16 @@ module.exports = {
   // add your custom rules here
   rules: {
     'prefer-promise-reject-errors': 'off',
+    'vue/require-default-prop': 'off',
+
+    "vue/max-attributes-per-line": ["warn", {
+      "singleline": {
+        "max": 4
+      },
+      "multiline": {
+        "max": 1
+      }
+    }],
 
     // TypeScript
     quotes: ['warn', 'single', { avoidEscape: true }],
