04e3fc4cafa72bbb9f9e922621540ac9074aa30f
Christoph Beger <ChristophB@users.noreply.github.com>
2021-10-29 09:30:02 +0000

    Add more example entities

--- src/api/entityRepository.ts
+++ src/api/entityRepository.ts
@@ -2,6 +2,29 @@ import { DataType, EntityType, IEntity } from 'src/components/models'
 
 const _entites: IEntity[] = [
   {
+    id: 'anthropometry',
+    title: 'Anthropometry',
+    titles: [
+      { lang: 'de', text: 'Anthropometrie' },
+      { lang: 'en', text: 'Anthropometry' }
+    ],
+    entityType: EntityType.Category,
+    synonyms: [],
+    descriptions: []
+  },
+  {
+    id: 'bmi',
+    title: 'BMI',
+    titles: [
+      { lang: 'de', text: 'BMI' },
+      { lang: 'en', text: 'BMI' }
+    ],
+    entityType: EntityType.SinglePhenotype,
+    dataType: DataType.Number,
+    synonyms: [],
+    descriptions: []
+  },
+  {
     id: 'height',
     title: 'Height',
     titles: [
@@ -15,7 +38,30 @@ const _entites: IEntity[] = [
       { lang: 'de', text: 'Beispielbeschreibung' },
       { lang: 'en', text: 'Example description' }
     ]
-  }
+  },
+  {
+    id: 'weight',
+    title: 'Weight',
+    titles: [
+      { lang: 'de', text: 'Gewicht' },
+      { lang: 'en', text: 'Weight' }
+    ],
+    entityType: EntityType.SinglePhenotype,
+    dataType: DataType.Number,
+    synonyms: [],
+    descriptions: []
+  },
+  {
+    id: 'laboratory_values',
+    title: 'Laboratory Values',
+    titles: [
+      { lang: 'de', text: 'Laborwerte' },
+      { lang: 'en', text: 'Laboratory Values' }
+    ],
+    entityType: EntityType.Category,
+    synonyms: [],
+    descriptions: []
+  },
 ]
 
 // eslint-disable-next-line @typescript-eslint/require-await
