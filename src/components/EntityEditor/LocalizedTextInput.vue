5b9220bfc30b30ff491c3a08824599df7993cd0f
Christoph Beger <ChristophB@users.noreply.github.com>
2021-11-29 13:53:51 +0000

    Make localized text component expandable

--- src/components/EntityEditor/LocalizedTextInput.vue
+++ src/components/EntityEditor/LocalizedTextInput.vue
@@ -1,7 +1,8 @@
 <template>
   <q-card>
-    <q-card-section v-if="label" class="q-pa-sm">
+    <q-card-section v-if="label" class="q-pa-sm" @click="isExpanded = !isExpanded">
       <q-toolbar>
+        <q-btn color="grey" round flat dense :icon="isExpanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'" />
         <q-toolbar-title>{{ label }}</q-toolbar-title>
         <q-btn
           v-if="helpText"
@@ -10,68 +11,72 @@
           dense
           icon="info"
           :title="t('showThing', { thing: t('help') })"
-          @click="showHelp = !showHelp"
+          @click.stop="showHelp = !showHelp; isExpanded = showHelp || isExpanded"
         />
       </q-toolbar>
     </q-card-section>
 
-    <q-separator v-if="label" />
+    <q-slide-transition>
+      <div v-show="isExpanded">
+        <q-separator v-if="label" />
 
-    <q-card-section class="row q-pa-none">
-      <div class="col q-pa-md">
-        <div v-for="(entry, index) in modelValue" :key="index" class="row">
-          <q-input
-            :model-value="modelValue[index].text"
-            :type="textArea ? 'textarea' : 'text'"
-            :rows="rows"
-            :cols="cols"
-            debounce="200"
-            @update:modelValue="updateEntryByIndex(index, $event, modelValue[index].lang)"
-          >
-            <template #before>
-              <q-select
-                :model-value="modelValue[index].lang"
-                :options="supportedLangs"
-                @update:modelValue="updateEntryByIndex(index, modelValue[index].text, $event)"
-              />
-            </template>
-            <template #after>
-              <q-btn
-                color="red"
-                icon="remove"
-                class="remove-localized-text-btn"
-                :title="t('remove')"
-                @click="removeEntryByIndex(index)"
-              />
-            </template>
-          </q-input>
-        </div>
-        <div v-show="uniqueLangs && duplicatedLangs.length > 0">
-          {{ t('oneThingPerThing', { thing1: name || t('entry'), thing2: t('language') }) }}
-        </div>
+        <q-card-section class="row q-pa-none">
+          <div class="col q-pa-md">
+            <div v-for="(entry, index) in modelValue" :key="index" class="row">
+              <q-input
+                :model-value="modelValue[index].text"
+                :type="textArea ? 'textarea' : 'text'"
+                :rows="rows"
+                :cols="cols"
+                debounce="200"
+                @update:modelValue="updateEntryByIndex(index, $event, modelValue[index].lang)"
+              >
+                <template #before>
+                  <q-select
+                    :model-value="modelValue[index].lang"
+                    :options="supportedLangs"
+                    @update:modelValue="updateEntryByIndex(index, modelValue[index].text, $event)"
+                  />
+                </template>
+                <template #after>
+                  <q-btn
+                    color="red"
+                    icon="remove"
+                    class="remove-localized-text-btn"
+                    :title="t('remove')"
+                    @click="removeEntryByIndex(index)"
+                  />
+                </template>
+              </q-input>
+            </div>
+            <div v-show="uniqueLangs && duplicatedLangs.length > 0">
+              {{ t('oneThingPerThing', { thing1: name || t('entry'), thing2: t('language') }) }}
+            </div>
+          </div>
+
+          <q-separator v-show="showHelp" vertical />
+
+          <div v-show="showHelp" class="col-6 q-pa-md">
+            <div class="text-subtitle1">
+              {{ t('help') }}:
+            </div>
+            {{ helpText }}
+          </div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions>
+          <q-btn
+            color="primary"
+            icon="add"
+            class="add-localized-text-btn"
+            :label="t('addThing', { thing: name || t('entry') })"
+            @click="addEntry()"
+          />
+        </q-card-actions>
       </div>
-
-      <q-separator v-show="showHelp" vertical />
-
-      <div v-show="showHelp" class="col-6 q-pa-md">
-        <div class="text-subtitle1">
-          {{ t('help') }}:
-        </div>
-        {{ helpText }}
-      </div>
-    </q-card-section>
-
-    <q-separator />
-
-    <q-card-actions>
-      <q-btn
-        color="primary"
-        icon="add"
-        class="add-localized-text-btn"
-        :label="t('addThing', { thing: name || t('entry') })"
-        @click="addEntry()"
-      />
-    </q-card-actions>
+    </q-slide-transition>
   </q-card>
 </template>
 
@@ -95,13 +100,15 @@ export default defineComponent({
     supportedLangs: {
       type: Array as () => string[],
       default: () => ['de', 'en']
-    }
+    },
+    expanded: Boolean
   },
   emits: ['update:modelValue'],
   setup (props, { emit }) {
     // eslint-disable-next-line @typescript-eslint/unbound-method
     const { t } = useI18n()
     const showHelp = ref(false)
+    const isExpanded = ref(props.expanded)
 
     const duplicatedLangs = computed((): (string|undefined)[] => {
       return props.modelValue
@@ -114,7 +121,7 @@ export default defineComponent({
     })
 
     return {
-      t, showHelp, duplicatedLangs,
+      t, showHelp, duplicatedLangs, isExpanded,
       
       removeEntryByIndex (index: number) {
         let newModelValue = props.modelValue.slice()
