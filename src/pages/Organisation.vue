7e917c71c4ff8f4f5ae3efc129a3f2b57fb3bb25
Christoph Beger <ChristophB@users.noreply.github.com>
2022-02-18 11:23:34 +0000

    Embed repository components in organisation page

--- src/pages/Organisation.vue
+++ src/pages/Organisation.vue
@@ -4,6 +4,13 @@
       <q-card-section>
         <div class="text-h6">
           {{ organisation.name }}
+          <q-btn
+            class="float-right"
+            color="primary"
+            icon="add"
+            :title="t('createThing', { thing: t('repository') })"
+            @click="repository = newRepository(); showForm = true"
+          />
         </div>
         <small>{{ t('createdAt') }}: {{ d(organisation.createdAt, 'long') }}</small>
       </q-card-section>
@@ -11,6 +18,21 @@
         {{ organisation.description }}
       </q-card-section>
     </q-card>
+
+    <repository-table
+      :repositories="repositories"
+      :loading="repositoriesLoading"
+      @edit-clicked="repository = $event; showForm = true"
+      @row-clicked="routeToEditor($event)"
+    />
+
+    <repository-form
+      v-model="repository"
+      v-model:show="showForm"
+      :loading="saving"
+      @update:model-value="saveRepository($event)"
+      @delete-clicked="deleteRepository($event)"
+    />
   </q-page>
   <q-page else>
     <q-inner-loading showing :label="t('pleaseWait') + '...'" />
@@ -23,11 +45,19 @@ import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
 import useAlert from 'src/mixins/useAlert'
 import { OrganisationApiKey } from 'src/boot/axios'
-import { Organisation } from '@onto-med/top-api'
+import { RepositoryApiKey } from 'src/boot/axios'
+import { Organisation, Repository } from '@onto-med/top-api'
+import RepositoryTable from 'src/components/Repository/RepositoryTable.vue'
+import RepositoryForm from 'src/components/Repository/RepositoryForm.vue'
+import { AxiosResponse } from 'axios'
+import { v4 as uuidv4 } from 'uuid'
 
 export default defineComponent({
   name: 'Editor',
-  components: { },
+  components: {
+    RepositoryTable,
+    RepositoryForm
+  },
   props: {
     organisationId: {
       type: String,
@@ -36,16 +66,39 @@ export default defineComponent({
   },
   setup (props) {
     // eslint-disable-next-line @typescript-eslint/unbound-method
-    const { t, d }     = useI18n()
+    const { t, d } = useI18n()
     const { alert } = useAlert()
     const router = useRouter()
-    const loading   = ref(false)
+    const loading = ref(false)
+    const showForm = ref(false)
+    const saving = ref(false)
+    const repositoriesLoading = ref(false)
     const organisationApi = inject(OrganisationApiKey)
+    const repositoryApi = inject(RepositoryApiKey)
     const organisation = ref<Organisation|undefined>(undefined)
+    const repositories = ref<Repository[]>([])
+    const newRepository = () => {
+      return { id: (uuidv4 as () => string)() } as Repository
+    }
+    const repository = ref(newRepository())
+
+    const reloadRepositories = async () => {
+      if (!repositoryApi) return
+      repositoriesLoading.value = true
+
+      await repositoryApi.getRepositoriesByOrganisationId(props.organisationId)
+        .then(r => {
+          console.log(r.data)
+          repositories.value = r.data
+        })
+        .catch((e: Error) => alert(e.message))
+        .finally(() => repositoriesLoading.value = false)
+    }
 
     const reload = async () => {
       if (!organisationApi) return
       loading.value = true
+      
       await organisationApi.getOrganisationById(props.organisationId)
         .then(r => organisation.value = r.data)
         .catch((e: Error) => {
@@ -53,6 +106,8 @@ export default defineComponent({
           void router.push({ name: 'organisations' })
         })
         .finally(() => loading.value = false)
+      
+      await reloadRepositories()
     }
 
     onMounted(async () => {
@@ -63,8 +118,51 @@ export default defineComponent({
       t,
       d,
       alert,
+      showForm,
       organisation,
-      loading
+      repository,
+      repositories,
+      loading,
+      repositoriesLoading,
+      saving,
+      newRepository,
+      routeToEditor (repository: Repository) {
+        if (!organisation.value) return
+        void router.push({ name: 'editor', params: { organisationId: organisation.value.id, repositoryId: repository.id } })
+      },
+      async saveRepository (repository: Repository) {
+        if (!repositoryApi || !organisation.value) return
+        saving.value = true
+
+        let promise: Promise<AxiosResponse<Repository>>
+        if (repository.createdAt) {
+          promise = repositoryApi.updateRepositoryById(organisation.value.id, repository.id, repository)
+        } else {
+          promise = repositoryApi.createRepository(organisation.value.id, repository)
+        }
+
+        await promise
+          .then(() => {
+            showForm.value = false
+            alert(t('thingSaved', { thing: t('repository') }), 'positive')
+            void reload()
+          })
+          .catch((e: Error) => alert(e.message))
+          .finally(() => saving.value = false)
+      },
+      async deleteRepository (repository: Repository) {
+        if (!organisationApi || !organisation.value) return
+        saving.value = true
+
+        await repositoryApi?.deleteRepositoryById(repository.id, organisation.value.id)
+          .then(() => {
+            showForm.value = false
+            alert(t('thingDeleted', { thing: t('repository') }), 'positive')
+            void reload()
+          })
+          .catch((e: Error) => alert(e.message))
+          .finally(() => saving.value = false)
+      }
     }
   }
 })
