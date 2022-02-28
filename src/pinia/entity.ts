5b930fd43f6030ce0c3dc7ad12c9d889b2f2eab7
Christoph Beger <ChristophB@users.noreply.github.com>
2022-02-28 15:42:12 +0000

    Add pinia config

---
+++ src/pinia/entity.ts
@@ -0,0 +1,26 @@
+import { Entity, EntityApi } from '@onto-med/top-api'
+import { defineStore } from 'pinia'
+
+const entityApi = new EntityApi()
+
+export const useEntity = defineStore('entity', {
+  state: () => {
+    return {
+      entities: [] as Entity[]
+    }
+  },
+  actions: {
+    async reloadEntities (organisationId: string, repositoryId: string) {
+      if (!organisationId || !repositoryId)
+        throw {
+          name: 'MissingParametersException',
+          message: 'organisationId or repositoryId missing'
+        }
+      await entityApi.getEntitiesByRepositoryId(organisationId, repositoryId)
+        .then((r) => this.entities = r.data)
+    },
+    addEntity (entity: Entity) {
+      this.entities.push(entity)
+    }
+  }
+})
