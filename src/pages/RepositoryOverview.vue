4565bd03edd41b6175dc24f1cc070fd4605ee538
Christoph Beger <ChristophB@users.noreply.github.com>
2022-02-28 10:54:28 +0000

    Show organisation in table of repositories

--- src/pages/RepositoryOverview.vue
+++ src/pages/RepositoryOverview.vue
@@ -13,14 +13,22 @@
 
     <table-with-actions
       :rows="repositories"
+      :columns="columns"
       :loading="loading"
       @row-clicked="routeToRepository($event)"
-    />
+    >
+      <template #row-cells="{ row }">
+        <q-td>{{ row.organisation.name || row.organisation.id }}</q-td>
+        <q-td>{{ row.name || row.id }}</q-td>
+        <q-td>{{ row.description }}</q-td>
+        <q-td>{{ d(row.createdAt, 'long') }}</q-td>
+      </template>
+    </table-with-actions>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent, inject, ref, onMounted } from 'vue'
+import { defineComponent, inject, ref, onMounted, computed } from 'vue'
 import { useI18n } from 'vue-i18n'
 import useAlert from 'src/mixins/useAlert'
 import { useRouter } from 'vue-router'
@@ -36,12 +44,18 @@ export default defineComponent({
   },
   setup () {
     // eslint-disable-next-line @typescript-eslint/unbound-method
-    const { t }     = useI18n()
+    const { t, d }  = useI18n()
     const { alert } = useAlert()
     const router    = useRouter()
     const loading   = ref(false)
     const repositoryApi = inject(RepositoryApiKey)
     const repositories  = ref<Repository[]>([])
+    const columns = computed(() => [
+      { name: 'organisation', label: t('organisation'), align: 'left', sortable: true },
+      { name: 'name', field: 'name', label: t('name'), align: 'left', required: true, sortable: true },
+      { name: 'description', field: 'description', label: t('description'), align: 'left', sortable: true },
+      { name: 'createdAt', field: 'createdAt', label: t('createdAt'), align: 'left', sortable: true }
+    ])
 
     const reload = async () => {
       if (!repositoryApi) return
@@ -58,7 +72,9 @@ export default defineComponent({
 
     return {
       t,
+      d,
       alert,
+      columns,
       repositories,
       loading,
       routeToRepository (repository: Repository) {
