0a92c801582f9a0f1fda716e766a2c996266ee3c
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-05-16 11:06:11 +0200

    Hide repository filter if user is not logged in

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -32,7 +32,7 @@
             clear-on-select
             show-details
             fork
-            repository-filter
+            :repository-filter="keycloak.authenticated"
             class="q-mr-sm fit"
             @entity-selected="routeToEntity"
             @fork-clicked="forkEntity"
