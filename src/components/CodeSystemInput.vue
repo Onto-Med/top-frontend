3811e629d9ee0a2fb654044d24857d35b50f14a9
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-06-08 16:35:31 +0200

    Simplify conde system input field

--- src/components/CodeSystemInput.vue
+++ src/components/CodeSystemInput.vue
@@ -3,19 +3,10 @@
     :model-value="modelValue"
     :options="options"
     :readonly="readonly"
+    option-value="uri"
+    option-label="name"
     @update:modelValue="$emit('selectionChanged', $event)"
-  >
-    <template #selected-item="{ opt }">
-      <span :title="opt.uri">{{ opt.name }}</span>
-    </template>
-    <template #option="scope">
-      <q-item v-bind="scope.itemProps" :title="scope.opt.uri">
-        <q-item-section>
-          <q-item-label>{{ scope.opt.name }}</q-item-label>
-        </q-item-section>
-      </q-item>
-    </template>
-  </q-select>
+  />
 </template>
 
 <script lang="ts">
