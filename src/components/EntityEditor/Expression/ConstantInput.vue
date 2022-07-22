688091830abbaa093610a05c2805f8b400cd015e
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-07-22 10:08:19 +0200

    Fix appending content befinde constant input

--- src/components/EntityEditor/Expression/ConstantInput.vue
+++ src/components/EntityEditor/Expression/ConstantInput.vue
@@ -1,39 +1,37 @@
 <template>
-  <div>
-    <div class="clickable">
-      <span v-show="expand">{{ '&nbsp;'.repeat((indentLevel) * indent) }}</span>
-      <span>{{ defaultConstant ? defaultConstant.title || defaultConstant.id : modelValue ? modelValue : '[' + t('constant') + ']' }}</span>
-    </div>
-    <q-popup-edit
-      v-if="!readonly"
-      ref="popup"
-      v-slot="scope"
-      :model-value="modelValue"
-      :cover="false"
-      auto-save
-      @update:model-value="$emit('update:modelValue', $event)"
-    >
-      <q-input v-model="scope.value" :type="dataType" dense autofocus @keyup.enter="scope.set">
-        <template #append>
-          <q-icon v-close-popup clickable name="check" class="cursor-pointer" />
-        </template>
-      </q-input>
-      <q-list v-if="constants" dense>
-        <q-item v-for="constant in constants" :key="constant.id" v-close-popup clickable @click="$emit('update:modelValue', constant.id)">
-          {{ constant.title || constant.id }}
-        </q-item>
-      </q-list>
-      <q-separator />
-      <q-list dense>
-        <q-item clickable @click="$emit('enclose')">
-          <q-item-section v-t="'encloseWithExpression'" />
-        </q-item>
-        <q-item clickable @click="$emit('remove')">
-          <q-item-section v-t="'remove'" />
-        </q-item>
-      </q-list>
-    </q-popup-edit>
+  <div class="clickable">
+    <span v-show="expand">{{ '&nbsp;'.repeat((indentLevel) * indent) }}</span>
+    <span>{{ defaultConstant ? defaultConstant.title || defaultConstant.id : modelValue ? modelValue : '[' + t('constant') + ']' }}</span>
   </div>
+  <q-popup-edit
+    v-if="!readonly"
+    ref="popup"
+    v-slot="scope"
+    :model-value="modelValue"
+    :cover="false"
+    auto-save
+    @update:model-value="$emit('update:modelValue', $event)"
+  >
+    <q-input v-model="scope.value" :type="dataType" dense autofocus @keyup.enter="scope.set">
+      <template #append>
+        <q-icon v-close-popup clickable name="check" class="cursor-pointer" />
+      </template>
+    </q-input>
+    <q-list v-if="constants" dense>
+      <q-item v-for="constant in constants" :key="constant.id" v-close-popup clickable @click="$emit('update:modelValue', constant.id)">
+        {{ constant.title || constant.id }}
+      </q-item>
+    </q-list>
+    <q-separator />
+    <q-list dense>
+      <q-item clickable @click="$emit('enclose')">
+        <q-item-section v-t="'encloseWithExpression'" />
+      </q-item>
+      <q-item clickable @click="$emit('remove')">
+        <q-item-section v-t="'remove'" />
+      </q-item>
+    </q-list>
+  </q-popup-edit>
 </template>
 
 <script lang="ts">
