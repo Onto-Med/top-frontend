8745ab2ee78e35b9566dfa5ed2f8ce1b7eff76b9
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-09-02 16:29:39 +0200

    Set up env vars for keycloak and axios

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -21,7 +21,7 @@ export const ForkApiKey: InjectionKey<ForkApi> = Symbol('forkApi')
 
 export default boot(({ app }) => {
   const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8080'
+    baseURL: process.env.API_URL
   })
   axiosInstance.interceptors.request.use(config => {
     const keycloak = (app.config.globalProperties.$keycloak as KeycloakInstance)
