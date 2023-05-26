4dd51b579a58b13af34c62d6689767ef77509885
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2023-05-26 08:12:18 +0000

    Add tooltip and label to code system select field

--- src/components/CodeSystemInput.vue
+++ src/components/CodeSystemInput.vue
@@ -3,22 +3,33 @@
     :model-value="modelValue"
     :options="options"
     :readonly="readonly"
+    :label="t('codeSystem')"
+    stack-label
+    clearable
     option-value="uri"
     option-label="shortName"
     @update:model-value="$emit('update:modelValue', $event)"
-  />
+  >
+    <template #option="scope">
+      <q-item dense v-bind="scope.itemProps">
+        <q-item-section :title="scope.opt.name">
+          {{ scope.opt.shortName }}
+        </q-item-section>
+      </q-item>
+    </template>
+  </q-select>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { CodeSystem } from '@onto-med/top-api'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   name: 'CodeSystemInput',
   props: {
     modelValue: {
-      type: Object as () => CodeSystem,
-      required: true
+      type: Object as () => CodeSystem
     },
     options: {
       type: Array as () => CodeSystem[],
@@ -28,7 +39,8 @@ export default defineComponent({
   },
   emits: ['update:modelValue'],
   setup () {
-    return {}
+    const { t } = useI18n()
+    return { t }
   }
 })
 </script>
