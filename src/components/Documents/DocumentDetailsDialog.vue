32d96c23306274345543f005835676b9d178b3eb
Franz Matthies <franz.matthies@gmail.com>
2023-12-09 10:13:11 +0100

    made the DocumentDialog a little bigger and introduced linebreaks so one need not scroll horizontally. also removed the 'close' button as it's superfluous when clicking outside the Dialog closes it

--- src/components/Documents/DocumentDetailsDialog.vue
+++ src/components/Documents/DocumentDetailsDialog.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-dialog ref="dialogRef">
-    <q-card class="full-width">
+  <q-dialog ref="dialogRef" full-height>
+    <q-card style="width: 750px; max-width: 80vw;">
       <q-card-section>
         <div class="text-h6">
           <q-icon name="article" />
@@ -16,16 +16,6 @@
           <pre class="q-pa-sm q-ma-none" v-html="document.highlightedText" />
         </q-scroll-area>
       </q-card-section>
-
-      <q-separator />
-
-      <q-card-actions align="right">
-        <q-btn
-          v-close-popup
-          flat
-          :label="t('close')"
-        />
-      </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
@@ -33,21 +23,25 @@
 <script setup lang="ts">
 import { Document } from '@onto-med/top-api'
 import { useDialogPluginComponent } from 'quasar';
-import { useI18n } from 'vue-i18n'
 
 defineProps<{
   document: Document
 }>()
 
-const { t } = useI18n()
 const { dialogRef } = useDialogPluginComponent()
 </script>
 
 <style scoped lang="sass">
 .highlighted-text
-  height: 60vh
+  height: 85vh
   width: 100%
   pre
     font-family: inherit
     text-wrap: wrap
+    overflow-x: auto
+    white-space: pre-wrap
+    white-space: -moz-pre-wrap
+    white-space: -pre-wrap
+    white-space: -o-pre-wrap
+    word-wrap: break-word
 </style>
