b24b66ced48c1692ca7bd939475bdc988a270273
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2024-02-23 12:58:10 +0000

    Rearrange code and remove comments

--- src/components/Query/GridDialog.vue
+++ src/components/Query/GridDialog.vue
@@ -19,8 +19,7 @@
       -->
       <!-- buttons example -->
       <q-card-actions align="right">
-        <q-btn color="primary" label="OK" @click="onOKClick" />
-        <q-btn color="primary" label="Cancel" @click="onDialogCancel" />
+        <q-btn color="primary" :label="t('close')" @click="onDialogHide" />
       </q-card-actions>
     </q-card>
   </q-dialog>
@@ -33,25 +32,18 @@ import 'ag-grid-community/styles/ag-theme-quartz.css' // Theme
 import { AgGridVue } from 'ag-grid-vue3' // Vue Grid Logic
 import Papa from 'papaparse'
 import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 const props = defineProps<{
   csv: string
 }>()
 
-function colDef(fieldName: string) {
-  let type = undefined
-  if (fieldName == 'Id') {
-    type = 'idColumn'
-  }
-  if (fieldName.includes('(DATE)')) {
-    type = 'dateColumn'
-  }
-  return {
-    field: fieldName,
-    type
-  }
-}
+defineEmits([
+  ...useDialogPluginComponent.emits
+])
 
+const { t } = useI18n()
+const { dialogRef, onDialogHide } = useDialogPluginComponent()
 const rows = computed(() => Papa.parse(props.csv, { header: true, delimiter: ';' }).data as Array<Array<string>>)
 const colDefs = computed(() => {
   const keys = Object.keys(rows.value[0])
@@ -71,26 +63,17 @@ const columnTypes = {
   }
 }
 
-defineEmits([
-  // REQUIRED; need to specify some events that your
-  // component will emit through useDialogPluginComponent()
-  ...useDialogPluginComponent.emits
-])
-
-const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } = useDialogPluginComponent()
-// dialogRef      - Vue ref to be applied to QDialog
-// onDialogHide   - Function to be used as handler for @hide on QDialog
-// onDialogOK     - Function to call to settle dialog with "ok" outcome
-//                    example: onDialogOK() - no payload
-//                    example: onDialogOK({ /*...*/ }) - with payload
-// onDialogCancel - Function to call to settle dialog with "cancel" outcome
-
-// this is part of our example (so not required)
-function onOKClick() {
-  // on OK, it is REQUIRED to
-  // call onDialogOK (with optional payload)
-  onDialogOK()
-  // or with payload: onDialogOK({ ... })
-  // ...and it will also hide the dialog automatically
+function colDef(fieldName: string) {
+  let type = undefined
+  if (fieldName == 'Id') {
+    type = 'idColumn'
+  }
+  if (fieldName.includes('(DATE)')) {
+    type = 'dateColumn'
+  }
+  return {
+    field: fieldName,
+    type
+  }
 }
 </script>
