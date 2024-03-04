b619997ffa22b8b85b4c938acf548ddf5ddb5481
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2024-03-04 09:18:57 +0000

    Upgrade top-api to v0.10.0 snapshot

--- src/components/Documents/ConceptClusterForm.vue
+++ src/components/Documents/ConceptClusterForm.vue
@@ -91,9 +91,7 @@
           <q-card-section class="bg-grey-1">
             <div class="text-subtitle2">
               {{ t('document', 2) }}
-              <span v-if="selectedConcepts.length">
-                ({{ t('resultCount', documents.length) }})
-              </span>
+              <span v-if="selectedConcepts.length"> ({{ t('resultCount', documents.length) }}) </span>
             </div>
           </q-card-section>
 
@@ -106,12 +104,7 @@
             :items="documents"
             separator
           >
-            <q-item
-              :key="item.id"
-              class="cursor-pointer relative-position"
-              clickable
-              @click="chooseDocument(item.id)"
-            >
+            <q-item :key="item.id" class="cursor-pointer relative-position" clickable @click="chooseDocument(item.id)">
               <q-item-section>
                 <q-item-label class="document-item">
                   {{ item.name || item.id }}
@@ -139,7 +132,7 @@
 import { computed, inject, onMounted, ref, watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { DocumentApiKey, ConceptClusterApiKey, ConceptgraphsApiKey } from 'src/boot/axios'
-import { Document, ConceptCluster, PipelineResponseStatus } from '@onto-med/top-api'
+import { Document, ConceptCluster, PipelineResponseStatus, DocumentGatheringMode } from '@onto-med/top-api'
 import useNotify from 'src/mixins/useNotify'
 import DocumentDetailsDialog from 'components/Documents/DocumentDetailsDialog.vue'
 import { useQuasar } from 'quasar'
@@ -148,7 +141,7 @@ import { useEntity } from 'src/pinia/entity'
 import { storeToRefs } from 'pinia'
 
 interface ConceptColor {
-  'background-color': string,
+  'background-color': string
   color: string
 }
 
@@ -167,31 +160,76 @@ const conceptsLoading = ref(false)
 const documentsLoading = ref(false)
 const selectedConcepts = ref<number[]>([])
 const distinctColors = [
-  '#556b2f', '#228b22',
-  '#7f0000', '#483d8b', '#008b8b', '#cd853f',
-  '#4682b4', '#000080', '#8fbc8f', '#800080',
-  '#b03060', '#ff0000', '#ffa500', '#00ff00',
-  '#dc143c', '#00ffff', '#0000ff', '#f08080',
-  '#adff2f', '#ff00ff', '#1e90ff', '#f0e68c',
-  '#ffff54', '#dda0dd', '#90ee90', '#7b68ee',
-  '#00ff7f', '#ff1493', '#696969', '#d3d3d3'
+  '#556b2f',
+  '#228b22',
+  '#7f0000',
+  '#483d8b',
+  '#008b8b',
+  '#cd853f',
+  '#4682b4',
+  '#000080',
+  '#8fbc8f',
+  '#800080',
+  '#b03060',
+  '#ff0000',
+  '#ffa500',
+  '#00ff00',
+  '#dc143c',
+  '#00ffff',
+  '#0000ff',
+  '#f08080',
+  '#adff2f',
+  '#ff00ff',
+  '#1e90ff',
+  '#f0e68c',
+  '#ffff54',
+  '#dda0dd',
+  '#90ee90',
+  '#7b68ee',
+  '#00ff7f',
+  '#ff1493',
+  '#696969',
+  '#d3d3d3'
 ]
 const distinctColorsFont = [
-  '#ffffff', '#ffffff',
-  '#ffffff', '#ffffff', '#000000', '#000000',
-  '#000000', '#ffffff', '#000000', '#ffffff',
-  '#000000', '#000000', '#000000', '#000000',
-  '#000000', '#000000', '#ffffff', '#000000',
-  '#000000', '#000000', '#000000', '#000000',
-  '#000000', '#000000', '#000000', '#000000',
-  '#000000', '#000000', '#ffffff', '#000000'
+  '#ffffff',
+  '#ffffff',
+  '#ffffff',
+  '#ffffff',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#ffffff',
+  '#000000',
+  '#ffffff',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#ffffff',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#000000',
+  '#ffffff',
+  '#000000'
 ]
 const conceptColors: ConceptColor[] = []
 const selectedColors = ref<ConceptColor[]>([])
-const conceptMode = ref('exclusive')
+const conceptMode = ref(DocumentGatheringMode.Exclusive)
 const mostImportantNodes = ref(false)
 const splitterModel = ref(40)
 const processId = 'top-framework'
+const datasource = ref('')
 
 const conceptModeOptions = computed(() => [
   {
@@ -218,88 +256,86 @@ const mostImportantNodesOptions = computed(() => [
 
 const loading = computed(() => conceptsLoading.value || documentsLoading.value)
 
-onMounted(async () =>
-  await entityStore.loadUser()
-    .then(() => reloadConcepts())
-)
+onMounted(async () => await entityStore.loadUser().then(() => reloadConcepts()))
 
-watch(
-  [ mostImportantNodes, conceptMode, selectedConcepts ],
-  () => reloadDocuments().catch((e: Error) => renderError(e))
-)
+watch([mostImportantNodes, conceptMode, selectedConcepts], () => reloadDocuments().catch((e: Error) => renderError(e)))
 
 async function reloadConcepts() {
   if (!conceptApi || !documentApi || conceptsLoading.value) return
   conceptsLoading.value = true
   await checkPipeline()
-    .then(() => conceptApi.getConceptClusters(undefined, false))
-    .then(r => {
-      concepts.value = r.data
+    .then(() => conceptApi.getConceptClusters(undefined, undefined, false))
+    .then((r) => {
+      concepts.value = r.data.content
       concepts.value.forEach((_, index) => {
         conceptColors.push({
           'background-color': distinctColors[index],
-          'color': distinctColorsFont[index]
+          color: distinctColorsFont[index]
         })
       })
-      selectedColors.value = new Array(concepts.value.length)
-        .fill({ 'background-color': '', 'color': '' }) as ConceptColor[]
+      selectedColors.value = new Array(concepts.value.length).fill({
+        'background-color': '',
+        color: ''
+      }) as ConceptColor[]
     })
     .catch((e: Error) => renderError(e))
-    .finally(() => conceptsLoading.value = false)
+    .finally(() => (conceptsLoading.value = false))
 }
 
 async function reloadDocuments() {
   if (!documentApi || documentsLoading.value) return
   documents.value = []
   document_.value = undefined
-  const conceptClusterIds = selectedConcepts.value.map(c => concepts.value[c]?.id).filter(id => !!id)
+  const conceptClusterIds = selectedConcepts.value.map((c) => concepts.value[c]?.id).filter((id) => !!id)
   if (conceptClusterIds.length > 0) {
     documentsLoading.value = true
-    await documentApi.getDocumentIdsByConceptClusterIds(
-      conceptClusterIds,
-      conceptMode.value,
-      undefined,
-      mostImportantNodes.value
-    )
-      .then(r => documents.value = r.data)
+    await documentApi
+      .getDocuments(
+        datasource.value,
+        undefined,
+        undefined,
+        undefined,
+        conceptClusterIds,
+        undefined,
+        conceptMode.value,
+        mostImportantNodes.value
+      )
+      .then((r) => (documents.value = r.data.content))
       .catch((e: Error) => renderError(e))
-      .finally(() => documentsLoading.value = false)
+      .finally(() => (documentsLoading.value = false))
   }
 }
 
-function setConceptMode(newMode = 'exclusive') {
+function setConceptMode(newMode = DocumentGatheringMode.Exclusive) {
   if (newMode === conceptMode.value) return
   conceptMode.value = newMode
-  if (selectedConcepts.value.length > 1 && newMode === 'exclusive')
-    prepareSelectedConcepts()
+  if (selectedConcepts.value.length > 1 && newMode === DocumentGatheringMode.Exclusive) prepareSelectedConcepts()
 }
 
 function prepareSelectedConcepts() {
-  if (conceptMode.value === 'exclusive') {
+  if (conceptMode.value === DocumentGatheringMode.Exclusive) {
     selectedConcepts.value = [selectedConcepts.value.at(-1) as number]
   }
   selectedColors.value.fill({
     'background-color': '',
-    'color': ''
+    color: ''
   })
-  selectedConcepts.value.forEach(idx => selectedColors.value[idx] = conceptColors[idx])
+  selectedConcepts.value.forEach((idx) => (selectedColors.value[idx] = conceptColors[idx]))
 }
 
 async function checkPipeline() {
-  return await conceptGraphsApi?.getStoredProcesses()
-    .then(r =>
-      r.data
-        .filter((p) => p.name === processId && p.finished_steps?.some((s) => s.name === 'graph'))
-        .length > 0
-    ).then(r => !r ? Promise.reject() : true)
+  return await conceptGraphsApi
+    ?.getStoredProcesses()
+    .then(
+      (r) => r.data.filter((p) => p.name === processId && p.finished_steps?.some((s) => s.name === 'graph')).length > 0
+    )
+    .then((r) => (!r ? Promise.reject() : true))
 }
 
 async function chooseDocument(documentId: string) {
-  await documentApi?.getDocumentById(
-    documentId,
-    selectedConcepts.value.map(idx => concepts.value[idx].id)
-  )
-    .then(r => {
+  await documentApi
+    ?.getSingleDocumentById(documentId, datasource.value)
+    .then((r) => {
       $q.dialog({
         component: DocumentDetailsDialog,
         componentProps: {
@@ -318,8 +354,9 @@ function confirmRegenerate() {
       message: t('conceptCluster.confirmRegenerate')
     }
   }).onOk(() => {
-    conceptGraphsApi?.startConceptGraphPipelineWithoutUpload(processId)
-      .then(r => {
+    conceptGraphsApi
+      ?.startConceptGraphPipelineWithoutUpload(processId)
+      .then((r) => {
         if (r.data.status == PipelineResponseStatus.Successful) {
           concepts.value = []
           selectedColors.value = []
