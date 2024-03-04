f890dcb19b2fdde52f61221d0db39d5cf0ade885
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2024-03-04 12:37:32 +0000

    Show selected document query

--- src/pages/DocumentSearch.vue
+++ src/pages/DocumentSearch.vue
@@ -6,22 +6,25 @@
           <div class="text-h6">
             {{ t('documentSearch.title') }}
           </div>
-          <span class="q-pt-md">{{ t('documentSearch.description') }}</span>
-          <q-select
-            v-model="dataSource"
-            :options="dataSources"
-            :label="t('dataSource')"
-            :error="!dataSource"
-            hide-bottom-space
-          />
+          <div v-if="query">
+            {{ t('dataOfQuery', { thing: query.name || query.id }) }}
+          </div>
+          <div v-else>
+            <span class="q-pt-md">{{ t('documentSearch.description') }}</span>
+            <q-select
+              v-model="dataSource"
+              :options="dataSources"
+              :label="t('dataSource')"
+              :error="!dataSource"
+              hide-bottom-space
+            />
+          </div>
         </div>
       </q-card-section>
       <q-separator />
 
       <q-card-section class="q-pa-none">
-        <concept-cluster-form
-          :data-source="dataSource"
-        />
+        <concept-cluster-form :data-source="dataSource" />
       </q-card-section>
     </q-card>
   </q-page>
@@ -30,13 +33,13 @@
 <script setup lang="ts">
 import ConceptClusterForm from 'components/Documents/ConceptClusterForm.vue'
 import { useI18n } from 'vue-i18n'
-import { inject, onMounted, ref } from 'vue'
+import { inject, onMounted, ref, watch } from 'vue'
 import { SearchTypesEnum } from 'src/config'
-import { DataSource, QueryType } from '@onto-med/top-api'
+import { DataSource, Query, QueryType } from '@onto-med/top-api'
 import { QueryApiKey } from 'src/boot/axios'
 import useNotify from 'src/mixins/useNotify'
 
-defineProps({
+const props = defineProps({
   initialSearchType: {
     type: String as () => SearchTypesEnum,
     default: SearchTypesEnum.CONCEPT_CLUSTER
@@ -52,13 +55,30 @@ const { renderError } = useNotify()
 const dataSource = ref<DataSource>()
 const dataSources = ref<DataSource[]>([])
 const queryApi = inject(QueryApiKey)
+const query = ref<Query>()
 
-onMounted(() => reloadDataSources())
+watch(
+  () => props.queryId,
+  () => loadQuery()
+)
+
+onMounted(() => {
+  loadQuery()
+  reloadDataSources()
+})
 
 function reloadDataSources() {
   queryApi
     ?.getDataSources(QueryType.Concept)
     .then((r) => (dataSources.value = r.data))
     .catch((e: Error) => renderError(e))
+}
+
+function loadQuery() {
+  if (props.organisationId && props.repositoryId && props.queryId)
+    queryApi
+      ?.getQueryById(props.organisationId, props.repositoryId, props.queryId)
+      .then((r) => (query.value = r.data))
+      .catch((e: Error) => renderError(e))
 }
 </script>
