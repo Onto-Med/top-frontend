0f6098843275f55a80ba66eae5485198f12a8056
Franz Matthies <franz.matthies@imise.uni-leipzig.de>
2024-04-15 15:53:24 +0200

    Improve phrase marking in documents
    Now foreground color is also provided

--- src/components/Documents/DocumentSearchForm.vue
+++ src/components/Documents/DocumentSearchForm.vue
@@ -396,7 +396,7 @@ async function chooseDocument(document: Document) {
   if (!props.dataSource) return Promise.reject()
   const conceptIds = new Array<string>();
   for (const c of selectedConcepts.value) {
-    conceptIds.push('$color::' + conceptColors[c]['background-color'] + '::color$' + c)
+    conceptIds.push('$color::' + conceptColors[c]['background-color'] + '|' + conceptColors[c]['color'] + '::color$' + concepts.value[c].id)
   }
   console.log(conceptIds)
   await documentApi
