ecd6a807ab71ac78e4524ed44a8a2e56e933692b
Franz Matthies <franz.matthies@imise.uni-leipzig.de>
2024-08-14 15:27:43 +0200

    loads default config from concept-graphs-api

--- src/components/Documents/PipelineConfigForm.vue
+++ src/components/Documents/PipelineConfigForm.vue
@@ -1,93 +1,76 @@
 <template>
   <q-dialog ref="dialogRef">
-    <q-card class="content">
-      <q-card-section class="row items-center q-gutter-sm">
-        <div class="col text-h6">
-          {{ t('configurationOfPipeline') }}
-        </div>
-        <q-btn v-close-popup icon="close" flat round dense />
-      </q-card-section>
-
-      <q-separator />
-      <q-card-section class="q-pa-none">
-        <JsonEditorVue
-          v-model="jsonConfig"
-          v-bind="{/* local props & attrs */}"
-        />
-      </q-card-section>
-    </q-card>
+      <q-layout view="lhh LpR lff" container style="height: 90%">
+        <q-page-container>
+          <q-page style="padding-top: 50px">
+            <JsonEditorVue
+              v-model="jsonConfig"
+              v-bind="{/* local props & attrs */}"
+            />
+          </q-page>
+          <q-page-sticky expand position="top">
+            <q-toolbar class="bg-secondary text-white">
+              <q-toolbar-title>
+                {{ t('configurationOfPipeline') }}
+              </q-toolbar-title>
+            </q-toolbar>
+          </q-page-sticky>
+          <q-page-sticky position="bottom">
+            <q-btn-group style="padding: 2px" >
+              <q-btn
+                label="Save"
+                color="secondary"
+                @click="saveConfig"
+              />
+            </q-btn-group>
+          </q-page-sticky>
+        </q-page-container>
+      </q-layout>
   </q-dialog>
 </template>
 
 <script setup lang="ts">
 import JsonEditorVue from 'json-editor-vue'
-import { ref } from 'vue'
+import { inject, onMounted, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
+import { ConceptPipelineApiKey } from 'boot/axios'
+import useNotify from 'src/mixins/useNotify'
 
+const { renderError } = useNotify()
+const conceptPipelineApi = inject(ConceptPipelineApiKey)
 const { t } = useI18n()
-const jsonConfig = ref({
-  'data': {
-    'spacy_model': 'de_dep_news_trf',
-    'n_process': 1,
-    'file_extension': 'txt',
-    'file_encoding': 'utf-8',
-    'use_lemma': false,
-    'prepend_head': false,
-    'head_only': false,
-    'case_sensitive': false,
-    'tfidf_filter': {
-      'enabled': false,
-      'min_df': 1,
-      'max_df': 1.0,
-      'stop': false
-    },
-    'disable': null,
-    'negspacy': {
-      'enabled': true,
-      'configuration': {
-        'chunk_prefix': ['kein', 'keine', 'keinen'],
-        'neg_termset_file': './conf/negex_files/negex_trigger_german_biotxtm_2016_extended.txt',
-        'scope': 1,
-        'language': 'de',
-        'feat_of_interest': 'NC'
-      }
-    }
-  },
-  'embedding': {
-    'model': 'Sahajtomar/German-semantic',
-    'n_process': 1
-  },
-  'clustering': {
-    'algorithm': 'kmeans',
-    'downscale': 'umap',
-    'missing_as_recommended': true,
-    'scaling': {
-
-    },
-    'clustering': {
-
-    },
-    'kelbow': {
-
-    }
-  },
-  'graph': {
-    'cluster': {
-      'distance': 0.7,
-      'min_size': 4
-    },
-    'graph': {
-      'cosine_weight': 0.6,
-      'merge_threshold': 0.95,
-      'graph_weight_cut_off': 0.5,
-      'unroll': false,
-      'simplify': 0.5,
-      'simplify_alg': 'significance',
-      'sub_clustering': false
-    },
-    'restrict_to_cluster': true
+const jsonConfig = ref<string>('{ }')
+const props = defineProps({
+  config: {
+    type: String,
+    default: ''
   }
 })
+
+onMounted(() => {
+  loadConfig()
+})
+
+function loadConfig() {
+  if (props.config === '') {
+    let errStr = 'Couldn\'t find configuration. Please refer to the documentation on how to write your own.'
+    conceptPipelineApi?.getConceptGraphPipelineConfiguration()
+      .then((r) => {
+        if (!r.data || r.data === '' || r.data === '{}') {
+          jsonConfig.value = errStr
+        } else {
+          jsonConfig.value = r.data
+        }
+      })
+      .catch((e: Error) => {
+        jsonConfig.value = errStr
+        renderError(e)
+      })
+  }
+}
+
+function saveConfig() {
+}
 </script>
 
 <style scoped lang="sass">
