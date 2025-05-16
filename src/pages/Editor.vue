a963dd4e0efd2257380050a607fe28318a6ab206
Christoph Beger <ChristophB@users.noreply.github.com>
2025-05-16 08:40:13 +0200

    Hide repository test button in some cases (#376)
    - For concept repositories
    - For users without write permission

--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -25,6 +25,7 @@
                 <q-icon name="sync_alt" class="rotate-90" />
               </q-btn>
               <q-btn
+                v-if="writable && !isConceptRepository"
                 dense
                 :title="t('testThing', { thing: t('repository') })"
                 @click="showTestDialog()"
