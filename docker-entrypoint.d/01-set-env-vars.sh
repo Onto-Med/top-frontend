99db22b486c2d8a812c2b547038f98c2dbfea4d7
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2025-10-06 10:49:56 +0000

    Add RAG_ENABLED var to docker entrypoint script

--- docker-entrypoint.d/01-set-env-vars.sh
+++ docker-entrypoint.d/01-set-env-vars.sh
@@ -43,6 +43,11 @@ then
     sed -i "s|DOCUMENTS_ENABLED\\:!1|DOCUMENTS_ENABLED:${DOCUMENTS_ENABLED}|g" /usr/share/nginx/html/assets/*.js
 fi
 
+if [[ ! -z "${RAG_ENABLED}" ]]
+then
+    sed -i "s|RAG_ENABLED\\:!1|RAG_ENABLED:${RAG_ENABLED}|g" /usr/share/nginx/html/assets/*.js
+fi
+
 if [[ ! -z "${TOP_PHENOTYPIC_QUERY_DOC_BASE_URL}" ]]
 then
     sed -i "s|TOP_PHENOTYPIC_QUERY_DOC_BASE_URL\\:\"https\\://onto-med\\.github\\.io/top-phenotypic-query/care/smith/top/top_phenotypic_query/c2reasoner\"|TOP_PHENOTYPIC_QUERY_DOC_BASE_URL:'${TOP_PHENOTYPIC_QUERY_DOC_BASE_URL}'|g" /usr/share/nginx/html/assets/*.js
